6e95b7422bf4689c80fb5288b185d5bae16e24af
Jeferson <jefersonfbh2010@gmail.com>
2026-07-13 10:24:12 -0300

    Q6

--- Program.cs
+++ Program.cs
@@ -4,23 +4,53 @@ class Program
 {
     static void Main()
     {
-        int[,] matriz = new int[3,3];
-        int maior = int.MinValue;
+        // Vetor para guardar os nomes
+        string[] nomes = new string[5];
 
-        for (int linha = 0; linha < 3; linha++)
+        // Matriz para guardar as 3 notas de cada aluno
+        double[,] notas = new double[5, 3];
+
+        // Vetor para guardar a média
+        double[] medias = new double[5];
+
+        // Cadastro
+        for (int i = 0; i < 5; i++)
         {
-            for (int coluna = 0; coluna < 3; coluna++)
-            {
-                Console.Write("Digite um número: ");
-                matriz[linha,coluna] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nome do aluno: ");
+            nomes[i] = Console.ReadLine()!;
 
-                if (matriz[linha,coluna] > maior)
-                {
-                    maior = matriz[linha,coluna];
-                }
+            double soma = 0;
+
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write("Nota " + (j + 1) + ": ");
+                notas[i, j] = Convert.ToDouble(Console.ReadLine());
+
+                soma += notas[i, j];
             }
+
+            medias[i] = soma / 3;
         }
 
-        Console.WriteLine("\nMaior valor: " + maior);
+        Console.WriteLine("\n===== BOLETIM =====");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("\nAluno: " + nomes[i]);
+            Console.WriteLine("Média: " + medias[i].ToString("F2"));
+
+            if (medias[i] >= 7)
+            {
+                Console.WriteLine("Situação: Aprovado");
+            }
+            else if (medias[i] >= 5)
+            {
+                Console.WriteLine("Situação: Recuperação");
+            }
+            else
+            {
+                Console.WriteLine("Situação: Reprovado");
+            }
+        }
     }
 }
